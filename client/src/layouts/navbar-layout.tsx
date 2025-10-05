171ee7253a74442b5fee61a8e72ef6083b6997d9
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-10-06 00:59:06 +0530

    feat: add HoverCard and Popover components; update NavbarLayout to integrate user and cart features

--- client/src/layouts/navbar-layout.tsx
+++ client/src/layouts/navbar-layout.tsx
@@ -11,13 +11,35 @@ import {
 } from "@/components/ui/navigation-menu"
 import { categories } from "@/constants"
 import { Link } from "react-router"
-import { ShoppingCart, User } from "lucide-react"
+import { GalleryVerticalEnd, ShoppingCart, User } from "lucide-react"
 import { Input } from "@/components/ui/input"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
+import { Search } from 'lucide-react';
 
 export default function NavbarLayout() {
     return (
         <div className="w-full border-b py-4 px-20 flex justify-between items-center">
-            <div>Logo</div>
+
+            <div className="flex justify-center gap-2 md:justify-start">
+                <Link to="/" className="flex items-center gap-2 font-medium">
+                    <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                        <GalleryVerticalEnd className="size-4" />
+                    </div>
+                    <span className="text-lg font-bold">E-Comm</span>
+                </Link>
+            </div>
+
             <div className="flex items-center gap-4">
                 <NavigationMenu>
                     <NavigationMenuList>
@@ -44,13 +66,43 @@ export default function NavbarLayout() {
                         </NavigationMenuItem>
                     </NavigationMenuList>
                 </NavigationMenu>
-                <div>
+                <div className="flex items-center">
                     <Input type="text" placeholder="Search products..." className="w-96" />
+                    <Button className="ml-2 cursor-pointer"><Search /></Button>
                 </div>
             </div>
             <div className="flex gap-8 items-center">
-                <div><User /></div>
-                <div> <ShoppingCart /></div>
+
+                <Popover>
+                    <PopoverTrigger><User className="navbar-focus" /></PopoverTrigger>
+                    <PopoverContent className="mt-4 mr-4">
+                        <ul className="flex flex-col gap-2 py-2 ml-2">
+                            <li className="navbar-focus">
+                                <Link to="/orders">Orders</Link>
+                            </li>
+                            <li className="navbar-focus">
+                                <Link to="/wishlist">Wishlist</Link>
+                            </li>
+                            <li className="navbar-focus">
+                                <Link to="/saved-addresses">Saved Addresses</Link>
+                            </li>
+                            <Separator />
+                            <li className="navbar-focus font-semibold">
+                                <Link to="/edit-profile">Edit Profile</Link>
+                            </li>
+                            <li className="navbar-focus font-semibold">
+                                <Link to="/logout">Logout</Link>
+                            </li>
+                        </ul>
+                    </PopoverContent>
+                </Popover>
+
+                <HoverCard>
+                    <HoverCardTrigger><ShoppingCart className="navbar-focus" /></HoverCardTrigger>
+                    <HoverCardContent className="mt-4 mr-4">
+                        List Cart Items will be shown here
+                    </HoverCardContent>
+                </HoverCard>
             </div>
         </div>
     )
